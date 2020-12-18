bc6177ce070f41fc77eedd4935fd8172ecde034b
Uwe Jugel <uwe.jugel+gh@gmail.com>
2020-12-18 17:42:18 +0100

    scale up links and popup messages

--- google-meet-chat.js
+++ google-meet-chat.js
@@ -1,11 +1,26 @@
 // ==UserScript==
 // @name         Meet Chat Scaling
 // @namespace    https://github.com/ubunatic/userscripts
-// @version      0.2
+// @version      0.3
 // @description  Fix the tiny Meet chat!
 // @author       Uwe Jugel, @ubunatic
 // @match        https://meet.google.com/*
 // @grant        GM_addStyle
 // ==/UserScript==
 
-GM_addStyle(`span[role="tabpanel"] div, textarea[name="chatTextInput"] { font-size: 15pt !important; line-height: 20pt !important; }`);
+(function() {
+    'use strict';
+
+    const selectors = [
+        `span[role="tabpanel"] div`,           // regular chat message
+        `span[role="tabpanel"] a`,             // link in chat message
+        `textarea[name="chatTextInput"]`,      // chat input
+        `div[role="button"][tabindex="0"] div` // popup message
+    ]
+
+    const style = `{ font-size: 15pt !important; line-height: 20pt !important; }`
+
+    GM_addStyle(selectors.join(", ") + style);
+})();
+
+
